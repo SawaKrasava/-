90e1359fe3bd956b2a17106fbe69c8a77b0fe983
SawaKrasava <159466840+SawaKrasava@users.noreply.github.com>
2024-02-14 11:30:45 +0300

    Add files via upload
    Вывод слова crickets в консоль каждый кадр

---
+++ Crickets.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crickets : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Вывод слова в консоль каждый кадр
+        print("crickets");   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
